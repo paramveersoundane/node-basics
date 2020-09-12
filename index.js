5493f994a38668d7ce5799f26e393cb7a26a961b
paramveersoundane <paramveersoundane@gmail.com>
2020-09-12 17:04:20 +0530

    Node callbacks and Error handling

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
-var rect = require('./rectangle');
-var rect = {
-	perimeter: (x, y) => (2*(x+y)),
-	area: (x, y) => (x*y)
-};
+// Step:3
+var rect =require('./rectangle');
 
-function solveRect(l,b) {
-    console.log("Solving for rectangle with l = " + l + " and b = " + b);
+function solveRectangle(l,b){
+  console.log("Solving rectangle");
+  rect(l,b,(err,rectangle)=>{
+      if(err){
+          console.log("Error: " + err.message);
+      }
+      else{
+          console.log("the are of rectangle is:  " + l+ " " + b + " " + rectangle.area());
+      }
+  });
+  console.log("this statement is after the call to rect");
 
-    if (l <= 0 || b <= 0) {
-        console.log("Rectangle dimensions should be greater than zero:  l = "
-               + l + ",  and b = " + b);
-    }
-    else {
-	    console.log("The area of the rectangle is " + rect.area(l,b));
-	    console.log("The perimeter of the rectangle is " + rect.perimeter(l,b));
-    }
 }
 
-solveRect(2,4);
-solveRect(3,5);
-solveRect(0,5);
-solveRect(-3,5);
+solveRectangle(-12,32);
+solveRectangle(21,43);
+solveRectangle(21,4);
+solveRectangle(0,43);
+  // Step 3 END 
+   // Step2
+// var rect =require('./rectangle');
+   // Step 1
+// // var rect= {
+// //     perimeter:(x,y)=> 2*(x+y),
+// //     area: (x,y)=>x*y
+// // }
+   // Step 1 End
+// function solveRectangle(l,b){
+//    if(l<=0 || b<=0){
+//        console.log("rectangle dimensions are invalid");
+//    }
+//    else{
+//        console.log("Perimeter is " + rect.perimeter(l,b));
+//        console.log("Area is " + rect.area(l,b) + " ");
+//    }
+
+// }
+
+// solveRectangle(-12,32);
+// solveRectangle(21,43);
+ //Step 2 End
--- rectangle.js
+++ rectangle.js
@@ -1,3 +1,31 @@
-exports.perimeter =  (x, y) => (2*(x+y));
+//Step 3
+ // https://nodejs.org/api/errors.html#errors_class_error
+module.exports=(x,y,callback) =>{  // callback function pass as the third parameter
+    // simulating with help of settimeout instead of delay caused by mongoDB
+    if(x<=0 || y<=0){
+        setTimeout(()=> 
+            callback(new Error("rectangle dimensions are invalid"),
+                 null),
+            2000); // here second parameter will be ignored when callback was just received
+        console.log("rectangle dimensions are invalid");
+    }
+    else{
+        setTimeout(()=> 
+            callback(null,{
+                perimeter :(x,y)=> 2*(x+y),
+                area: (x,y)=> (x*y)
+            }
+                 ), // first param to null cause no error
+            2000); // here second parameter will be ignored when callback was just received
+    }
+}
+// callback is function when supplied in when module is called (first class functions)
+// callback create new Error object
 
-exports.area = (x, y) => (x*y);
+// Step 3 END
+
+
+// Step 2
+// exports.perimeter = (x,y)=> 2*(x+y);
+// exports.area= (x,y)=> (x*y);  // module.exports can also be written
+// Step 2 End
